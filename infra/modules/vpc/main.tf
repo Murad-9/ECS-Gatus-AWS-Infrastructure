adc300a4edd3ab6dff240e7ca7149d095dcf6bb5
Murad-9 <murad10ten@gmail.com>
2026-09-11 15:37:34 +0100

    fix: set VPC subnets across availability zones

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -9,12 +9,14 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
 
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-west-1b"
 
 }
 
